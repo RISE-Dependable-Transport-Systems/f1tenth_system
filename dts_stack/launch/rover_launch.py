bf3023d11b9e003b6b81a57fca242821c1319578
Ramana Avula <r2avula@gmail.com>
2023-05-10 23:23:47 +0200

    joy, rviz in ctrl_stat.; moved all others to rover

--- dts_stack/launch/rover_launch.py
+++ dts_stack/launch/rover_launch.py
@@ -8,38 +8,68 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
 
 def generate_launch_description():
     # get the directories
     dts_dir = get_package_share_directory('dts_stack')
     sllidar_dir = get_package_share_directory('sllidar_ros2')
-    
+    slam_toolbox_dir = get_package_share_directory('slam_toolbox')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
+
+    # args that can be set from the command line
+    model = LaunchConfiguration('model')
+    ekf_config = LaunchConfiguration('ekf_config')
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+        
     # args that can be set from the command line or a default will be used
     joy_la = DeclareLaunchArgument(
         'joy_config', default_value=os.path.join(dts_dir, 'config/joy_teleop.yaml'),
         description='Full path to params file')
+    robot_state_publisher_la = DeclareLaunchArgument(
+        'model', default_value=os.path.join(dts_dir, 'urdf/box_car_camera.urdf'),
+        description='Full path to robot urdf file')
+    robot_localization_la = DeclareLaunchArgument(
+        'ekf_config', default_value=os.path.join(dts_dir, 'config/ekf.yaml'),
+        description='Full path to ekf config file')        
     vesc_la = DeclareLaunchArgument(
         'vesc_config', default_value=os.path.join(dts_dir, 'config/vesc.yaml'),
         description='Full path to params file')
     mux_la = DeclareLaunchArgument(
         'mux_config', default_value=os.path.join(dts_dir, 'config/mux.yaml'),
         description='Full path to params file')
-        
-    # include another launch file
+    nav2_la = DeclareLaunchArgument(
+        'params_file', default_value=os.path.join(dts_dir, 'config/nav2_params_modified.yaml'),
+        description='Full path to nav2 params file')
+    use_sim_time_la = DeclareLaunchArgument(
+        'use_sim_time', default_value='false',
+        description='Use simulation/Gazebo clock')    
+    
+    # include launch files
     sllidar_s1_launch_file = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(sllidar_dir, 'launch/sllidar_s1_launch.py'))
     )
+    slam_toolbox_start = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(slam_toolbox_dir, 'launch/online_async_launch.py')]),
+        launch_arguments={
+            'params_file': params_file,
+            'use_sim_time': use_sim_time
+            }.items()
+    )
+    nav2_start = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(nav2_bringup_dir, 'launch/navigation_launch.py')]),
+        launch_arguments={
+            'params_file': params_file,
+            'use_sim_time': use_sim_time
+            }.items()
+    )
     
     # start nodes and use args to set parameters
-    joy_node = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy',
-        parameters=[LaunchConfiguration('joy_config')]
-    )
     joy_teleop_node = Node(
         package='joy_teleop',
         executable='joy_teleop',
@@ -70,26 +100,65 @@ def generate_launch_description():
         name='ackermann_mux',
         parameters=[LaunchConfiguration('mux_config')],
         remappings=[('ackermann_cmd_out', 'ackermann_drive')]
+    )    
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[{
+            'robot_description': Command(['xacro ', model]),
+            'use_sim_time': use_sim_time,
+        }]
     )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+        }]
+    )
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[{
+            'ekf_config': ekf_config,
+            'use_sim_time': use_sim_time
+        }]
+    )
+    twist_to_ackermann_node = Node(
+        package='dts_stack',
+        executable='twist_to_ackermann',
+        name='twist_to_ackermann',
+        output='screen'
+    )   
     
     # create launch description
     ld = LaunchDescription()
     
     # declare launch args
     ld.add_action(joy_la)
+    ld.add_action(robot_state_publisher_la)
+    ld.add_action(robot_localization_la)
+    ld.add_action(use_sim_time_la)    
+    ld.add_action(nav2_la)
     ld.add_action(vesc_la)
     ld.add_action(mux_la)
     
     # start nodes
-    ld.add_action(joy_node)
+    ld.add_action(sllidar_s1_launch_file)   
     ld.add_action(joy_teleop_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(robot_localization_node)
     ld.add_action(ackermann_to_vesc_node)
     ld.add_action(vesc_to_odom_node)
     ld.add_action(vesc_driver_node)
-    ld.add_action(ackermann_mux_node)
-    
-    # run another launch flie 
-    ld.add_action(sllidar_s1_launch_file)
+    ld.add_action(ackermann_mux_node)    
+    ld.add_action(twist_to_ackermann_node)        
+    ld.add_action(slam_toolbox_start)  
+    ld.add_action(nav2_start)
     
     return ld
-                
